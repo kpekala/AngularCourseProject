d4bcfa3fe3dcaefc0c2ca9d68ff8c20c0ca6b74a
Konrad Pękala <kgpekala@gmail.com>
2023-04-04 00:19:18 +0200

    section5 done

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput', {static: true}) nameInput!: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInput!: ElementRef;
+  @Output() onAddIngredient = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    let name = this.nameInput.nativeElement.value;
+    let amount = Number(this.amountInput.nativeElement.value);
+    let ingredient = new Ingredient(name, amount);
+    this.onAddIngredient.emit(ingredient);
+  }
 }
