b3ca040189a58383a5405a54e79b17366df42eab
Konrad Pękala <kgpekala@gmail.com>
2023-09-18 20:03:26 +0200

    add header change

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { Subject, catchError, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     kind: string,
@@ -16,6 +17,8 @@ export interface AuthResponseData {
     providedIn: 'root'
 })
 export class AuthService {
+
+    user = new Subject<User>();
     
     constructor(private http: HttpClient) {
     
@@ -26,16 +29,10 @@ export class AuthService {
             .post<AuthResponseData>(
                 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD4TvCla1RG_aFLJJY3Qwyg_6_2dIMf8Nw',
                 {email: email, password: password, returnSecureToken: true}
-            ).pipe(catchError(errorResponse => {
-                let errorMessage = 'An uknown error occured';
-                if(!errorResponse.error || !errorResponse.error.error){
-                    return throwError(() => new Error(errorMessage));
-                }
-                switch(errorResponse.error.error.message){
-                    case 'EMAIL_EXISTS' :
-                        errorMessage = 'This email exists already';
-                }
-                return throwError(() => new Error(errorMessage));
+            ).pipe(
+                catchError(this.handleError), 
+                tap((resData: AuthResponseData) => {
+                    this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
             }));
     }
 
@@ -44,6 +41,38 @@ export class AuthService {
             .post<AuthResponseData>(
                 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD4TvCla1RG_aFLJJY3Qwyg_6_2dIMf8Nw',
                 {email: email, password: password, returnSecureToken: true}
-            );
+            ).pipe(
+                catchError(this.handleError),
+                tap((resData: AuthResponseData) => {
+                    this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+            }));
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
+    }
+
+    private handleError(errorResponse: HttpErrorResponse) {
+        let errorMessage = 'An uknown error occured';
+        if(!errorResponse.error || !errorResponse.error.error){
+            return throwError(() => new Error(errorMessage));
+        }
+        switch(errorResponse.error.error.message){
+            case 'EMAIL_EXISTS' :
+                errorMessage = 'This email exists already.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
+            case 'INVALID_LOGIN_CREDENTIALS':
+                errorMessage = 'Login credentials are not correct.';
+                break;
+        }
+        return throwError(() => new Error(errorMessage));
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,21 @@
-import {Component, EventEmitter, OnDestroy, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {ViewState} from "../shared/enums";
 import { RecipesService } from '../recipes/recipes.service';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnDestroy {
+export class HeaderComponent implements OnDestroy, OnInit{
 
+  private userSub: Subscription;
   fetchRecipesSub: Subscription;
+  isAuthenticated = false;
 
-  constructor(private recipesService: RecipesService){
+  constructor(private recipesService: RecipesService, private authService: AuthService){
 
   }
   
@@ -26,5 +29,12 @@ export class HeaderComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.fetchRecipesSub.unsubscribe();
+    this.userSub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+        this.isAuthenticated = !!user;
+    });
   }
 }
