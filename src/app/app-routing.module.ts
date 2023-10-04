cbe68f5a4d7cb31686a5f671550ee19343741b57
kpekala <kgpekala@gmail.com>
2023-10-04 21:25:05 +0200

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipesService } from "./recipes/recipes.service";
-import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: 'shopping-list', pathMatch: 'full'}
-    
+    {path: '', redirectTo: 'shopping-list', pathMatch: 'full'},
+    {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(x => x.RecipesModule)}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -22,7 +21,6 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
