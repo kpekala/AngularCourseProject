4d37a8011d5f5756fb3572b5c2bfccb1e208a29b
Konrad Pekala <konrad.pekala@motorolasolutions.com>
2023-11-22 16:45:03 +0100

    add recipe controller

---
+++ backend/src/main/java/com/kpekala/recipes/recipe/model/RecipeController.java
@@ -0,0 +1,11 @@
+package com.kpekala.recipes.recipe.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RecipeController {
+
+
+}
